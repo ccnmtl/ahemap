21e4477c06fbd32caff8648a2e5d829725067900
Susan Dreher <sdreher@gmail.com>
2019-02-16 07:27:48 -0500

    Import institution data

---
+++ ahemap/main/forms.py
@@ -0,0 +1,69 @@
+import csv
+import io
+
+from django import forms
+from django.utils.encoding import force_text, DjangoUnicodeDecodeError
+
+
+class InstitutionImportForm(forms.Form):
+    COLUMN_COUNT = 38
+
+    INVALID_FILE_FORMAT = ("The selected file is not formatted properly. "
+                           "Please select a valid data file.")
+    INVALID_ENCODING = (
+        "The selected file is not encoded properly. Batch files must be "
+        "encoded using the UTF-8 standard to ensure special characters are "
+        "translated correctly.<br /><br /> The full error is:<br />{}.")
+
+    csvfile = forms.FileField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(InstitutionImportForm, self).__init__(*args, **kwargs)
+
+    def csvfile_reader(self):
+        csv_file = self.request.FILES['csvfile']
+        csv_file.seek(0)
+        as_string = io.StringIO(csv_file.read().decode('utf-8'))
+        return csv.reader(as_string)
+
+    def validate_column_count(self, row):
+        return len(row) == self.COLUMN_COUNT
+
+    def validate_encoding(self, row):
+        for col in row:
+            try:
+                force_text(col)
+            except DjangoUnicodeDecodeError as e:
+                return False, e
+
+        return True, ''
+
+    def clean(self):
+        cleaned_data = super(InstitutionImportForm, self).clean()
+        if 'csvfile' not in cleaned_data:
+            self._errors['csvfile'] = self.error_class([
+                'Please select a data file'])
+            return cleaned_data
+
+        self.validate_clean_data(cleaned_data)
+        return cleaned_data
+
+    def validate_clean_data(self, cleaned_data):
+        # do some rudimentary validation on the file
+        try:
+            for row in self.csvfile_reader():
+                if not self.validate_column_count(row):
+                    self._errors['csvfile'] = self.error_class([
+                        self.INVALID_FILE_FORMAT])
+                    break
+
+                valid, e = self.validate_encoding(row)
+                if not valid:
+                    self._errors['csvfile'] = self.error_class([
+                        self.INVALID_ENCODING.format(e)])
+                    break
+
+        except csv.Error:
+            self._errors['csvfile'] = self.error_class([
+                self.INVALID_FILE_FORMAT])
--- ahemap/main/views.py
+++ ahemap/main/views.py
@@ -1,8 +1,13 @@
 from django.conf import settings
+from django.contrib import messages
+from django.db import transaction
+from django.urls.base import reverse
 from django.utils.html import escape
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 from rest_framework import viewsets
 
+from ahemap.main.forms import InstitutionImportForm
 from ahemap.main.models import Institution
 from ahemap.main.serializers import InstitutionSerializer
 
@@ -21,6 +26,38 @@ class IndexView(TemplateView):
 
 class MapView(TemplateView):
     template_name = "main/map.html"
+
+
+class InstitutionImportView(FormView):
+    template_name = "main/institution_import.html"
+    form_class = InstitutionImportForm
+    total = 0
+
+    def get_form_kwargs(self):
+        kwargs = super(InstitutionImportView, self).get_form_kwargs()
+        kwargs['request'] = self.request
+        return kwargs
+
+    def get_success_url(self):
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            '{} institutions imported.'.format(self.total),
+            extra_tags='safe'
+        )
+
+        return reverse('institution-import-view')
+
+    @transaction.atomic
+    def form_valid(self, form):
+        self.total = 0
+        reader = form.csvfile_reader()
+        for (idx, row) in enumerate(reader):
+            if idx == 0:
+                continue  # skip the header
+            Institution.objects.update_or_create(row)
+            self.total += 1
+
+        return super(InstitutionImportView, self).form_valid(form)
 
 
 class InstitutionViewSet(viewsets.ModelViewSet):
