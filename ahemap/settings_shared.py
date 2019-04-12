67db6942cfbd1064926c6226e4ac39b71dd70aca
Susan Dreher <sdreher@gmail.com>
2019-04-12 13:39:46 -0400

    Set Django security options
    from the https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/

--- ahemap/settings_shared.py
+++ ahemap/settings_shared.py
@@ -87,13 +87,19 @@ ALLOWED_HOSTS = [
     'ahemap.veterans.columbia.edu',
 ]
 
+MIDDLEWARE += [  # noqa
+    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+]
+
 
 THUMBNAIL_SUBDIR = "thumbs"
 LOGIN_REDIRECT_URL = "/"
 
 TEMPLATES[0]['OPTIONS']['context_processors'].append(  # noqa
     'ahemap.main.views.django_settings')
-
+TEMPLATES[0]['OPTIONS']['context_processors'].append(  # noqa
+    'django.template.context_processors.csrf')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -106,3 +112,5 @@ REST_FRAMEWORK = {
     'PAGINATE_BY': 15,
     'DATETIME_FORMAT': '%m/%d/%y %I:%M %p'
 }
+
+SESSION_COOKIE_SECURE = True
